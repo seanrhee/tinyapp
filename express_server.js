9ca50dcc02977106dfe0bc1fb64c09504d56b740
Sean Lee <lee.sean.cw@gmail.com>
2022-09-14 20:58:08 +0000

    add urlDatabase.json in ./data + add logic for persistent database through restarts

--- express_server.js
+++ express_server.js
@@ -1,14 +1,13 @@
 const express = require('express')
+const urlDatabase = require('./data/urlDatabase.json')
+const fs = require('fs');
+
 
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 const generateRandomString = function() {
   let result = '';
@@ -27,16 +26,16 @@ app.get('/', (req, res) => {
   res.send("Hello!");
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html\n');
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const urlList = fs.readFileSync('./data/urlDatabase.json');
+  const parsedList = JSON.parse(urlList);
+
+  const templateVars = {urls: parsedList};
+
   res.render('urls_index', templateVars);
 });
 
@@ -45,14 +44,44 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  req.body.id = generateRandomString();
+  const {id, longURL} = req.body;
+
+  const urlList = fs.readFileSync('./data/urlDatabase.json');
+  const parsedList = JSON.parse(urlList);
+
+  parsedList[id] = longURL;
+
+  const newData = JSON.stringify(parsedList, null, 4);
+  fs.writeFile('./data/urlDatabase.json', newData, err => {
+    if (err) throw err;
+
+    console.log(`{ ${id}: ${longURL} } added to ./data/urlDatabase.json`);
+  });
+
+  res.redirect(`/urls/${id}`);
 });
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
+  const urlList = fs.readFileSync('./data/urlDatabase.json');
+  const parsedList = JSON.parse(urlList);
+  
+  const templateVars = { id: req.params.id, longURL: parsedList[req.params.id]}
+
   res.render("urls_show", templateVars)
 });
+
+app.get('/u/:id', (req, res) => {
+  const urlList = fs.readFileSync('./data/urlDatabase.json');
+  const parsedList = JSON.parse(urlList);
+
+  const longURL = parsedList[req.params.id];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.send("Invalid Short URL");
+  }
+})
 
 
 app.listen(PORT, () => {
